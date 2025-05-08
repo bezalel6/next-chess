b084876ccd4886bd53c53e6e8bb8194894e99703
bezalel6 <bezalel3250@gmail.com>
2025-05-08 11:56:39 +0300

    fully implement queueing and game matching

--- src/pages/socket.ts
+++ src/pages/socket.ts
@@ -1,5 +1,12 @@
-import { io } from "socket.io-client";
+import { io } from 'socket.io-client';
+import type { CustomSocket } from '../types/socket';
 
-const isBrowser = typeof window !== "undefined";
+const isBrowser = typeof window !== "undefined"
+const SOCKET_URL = process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000';
 
-export const socket = isBrowser ? io() : {} as ReturnType<typeof io>;
+export const socket: CustomSocket = isBrowser ? io(SOCKET_URL, {
+    autoConnect: true,
+    reconnection: true,
+    reconnectionAttempts: 5,
+    reconnectionDelay: 1000,
+}) : {} as CustomSocket;

8bb8b0025599f37429adcd45e6b208a424d03011
bezalel6 <bezalel3250@gmail.com>
2025-05-08 07:57:00 +0300

    Init

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import styles from "./index.module.css";
+import Board from "../components/board";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Next Chess</title>
+        <meta name="description" content="Play chess online" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <Board />
+        </div>
+      </main >
+    </>
+  );
+}

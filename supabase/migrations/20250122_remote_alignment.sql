b4bd940f3884458b519e320d9bd395a54d67a9da
bezalel6 <bezalel3250@gmail.com>
2025-08-22 19:54:43 +0300

    chore(db): add no-op migration 20250122 to reconcile remote migration history

---
+++ supabase/migrations/20250122_remote_alignment.sql
@@ -0,0 +1,7 @@
+-- Reconciliation migration to align local with remote history
+-- Remote has migration version 20250122 which is not present locally.
+-- This file is intentionally a no-op to satisfy version alignment.
+
+DO $$ BEGIN
+  -- no-op
+END $$;
